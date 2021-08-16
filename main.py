60a2ab839d2b87b157c862943cb39eeecb70631f
albert <ja.chao2010@hotmail.com>
2021-08-16 23:22:12 +0800

    uncomment

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class Comment(db.Model):
     parent_post = relationship('BlogPost', back_populates='comments')
 
 
-# db.create_all()
+db.create_all()
 
 #Creat admin-only decorator
 def admin_only(function):
